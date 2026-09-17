970c53b83a121120c348bcf0493a858eecb9a510
Seo yeon Kim <merock2tar@gmail.com>
2026-09-17 22:43:24 +0900

    feat: Week00 Mission 2 Dart 연습 및 Mission 3 Hello MovieLog 구현 완료

--- Kongmi/Week00/movielog/lib/main.dart
+++ Kongmi/Week00/movielog/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello MovieLog!'),
-        ),
-      ),
-    ),
-  );
+      home: Scaffold(body: Center(child: Text('Hello MovieLog!'))),
+    );
+  }
 }
